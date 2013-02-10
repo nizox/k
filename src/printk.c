705851e8e81a796061b4f03de96614f4620358f8
Pierre Wacrenier <pierre.wacrenier@gmail.com>
2013-02-10 13:33:04 +0100

    Added printk and many improvements
    - added printk and vprintk
    - improved number formating for itoa
    - putchar is a print module function
    - mimic write(2) for the screen
    
    Construction:
        Since gcc and clang no longer recognize the cdecl attribute
    and use a register-base call in their abi, it is impossible
    to implement a self-made stdarg for the kernel.
    Therefore, the -nostdinc option have been removed from the
    construction process in order to let the compiler use its
    builtin stdarg management.

---
+++ src/printk.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include "string.h"
+#include "screen.h"
+#include "print.h"
+
+void                vprintk(const char *fmt, va_list ap)
+{
+    char            buf[34];
+    char            *str;
+
+    for (; *fmt; ++fmt)
+      {
+        if (*fmt == '%')
+          {
+            switch (*(++fmt))
+              {
+                case 'c':
+                    putchar((char)va_arg(ap, int));
+                    break;
+                case 's':
+                    print((const char *)va_arg(ap, const char *));
+                    break;
+                case 'i':
+                case 'd':
+                    print(itoa((int)va_arg(ap, int), buf, 10));
+                    break;
+                case 'p':
+                case 'x':
+                    print(itoa((int)va_arg(ap, int), buf, 16));
+                    break;
+                case 'o':
+                    print(itoa((int)va_arg(ap, int), buf, 8));
+                    break;
+                case '%':
+                    putchar(*fmt);
+                    break;
+                default:
+                    putchar('%');
+                    break;
+              }
+          }
+        else
+            putchar(*fmt);
+      }
+}
+
+void                printk(const char *fmt, ...)
+{
+    va_list         ap;
+
+    va_start(ap, fmt);
+    vprintk(fmt, ap);
+    va_end(ap);
+}
