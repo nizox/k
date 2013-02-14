c28fbdd470b037045265aee74ec521433d91b9d7
PICHOT `Chipot' Fabien <pichot.fabien@gmail.com>
2013-02-14 19:56:48 +0100

    types: add intptr_t
    Useful for holding addresses and make arithmetics with them.

--- include/c/types.h
+++ include/c/types.h
@@ -13,8 +13,9 @@ typedef unsigned char       uint8_t;
 typedef unsigned short      uint16_t;
 typedef unsigned int        uint32_t;
 typedef unsigned long long  uint64_t;
+typedef uint64_t            intptr_t;
 
-typedef unsigned long size_t;
+typedef unsigned long       size_t;
 
 #ifdef __cplusplus
 }
