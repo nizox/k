99e727c6feaf6f225c3088abeacc23b71e7c0b85
Pierre Wacrenier <pierre.wacrenier@gmail.com>
2013-02-27 01:49:27 +0100

    [Video] Better code locality.

--- src/video.cpp
+++ src/video.cpp
@@ -4,6 +4,10 @@
 #include "c/screen.h"
 #include "c/print.h"
 
+//
+// video class
+//
+
 bool                video::initialized_ = false;
 
 video::video(colors fg, colors bg)
@@ -134,4 +138,55 @@ video::operator<<(const video::attribute& attr)
 {
     this->set_attr(attr);
     return *this;
+}
+
+//
+// video::attribute class
+//
+
+video::attribute::attribute(colors fg, colors bg)
+    : fg_(fg), bg_(bg)
+{}
+
+video::attribute::attribute(const attribute& src)
+{
+    if (&src != this)
+      {
+        this->fg_ = src.fg_;
+        this->bg_ = src.bg_;
+      }
+}
+
+video::attribute::~attribute()
+{}
+
+void
+video::attribute::set(colors fg, colors bg)
+{
+    this->set_fg(fg);
+    this->set_bg(bg);
+}
+
+void
+video::attribute::set_fg(colors fg)
+{
+    fg_ = fg;
+}
+
+void
+video::attribute::set_bg(colors bg)
+{
+    bg_ = bg;
+}
+
+video::colors
+video::attribute::get_fg() const
+{
+    return fg_;
+}
+
+video::colors
+video::attribute::get_bg() const
+{
+    return bg_;
 }
