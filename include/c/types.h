e444e2e0c66a165520a3db5e3237b184b0cbcec0
PICHOT `Chipot' Fabien <pichot.fabien@gmail.com>
2013-03-01 22:16:07 +0100

    types: add signed types.

--- include/c/types.h
+++ include/c/types.h
@@ -9,11 +9,14 @@ __C_BEGIN
  * See http://wiki.osdev.org/X86-64#Data_Types
  */
 
+typedef char                int8_t;
 typedef unsigned char       uint8_t;
+typedef short               int16_t;
 typedef unsigned short      uint16_t;
+typedef int                 int32_t;
 typedef unsigned int        uint32_t;
+typedef long long           int64_t;
 typedef unsigned long long  uint64_t;
-typedef uint64_t            intptr_t;
 
 typedef long long           intmax_t;
 typedef unsigned long long  uintmax_t;
