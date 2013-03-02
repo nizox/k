f9232224a0632cf6349cf5e4a1c245a544d22701
PICHOT `Chipot' Fabien <pichot.fabien@gmail.com>
2013-03-02 01:24:24 +0100

    memory: add forgotten member function.

--- src/memory.cpp
+++ src/memory.cpp
@@ -9,6 +9,10 @@ info::info(size_t amount, size_t page_size, pml4_entry *pml4, pdp_entry* pdp, pd
     : amount(amount), page_size(page_size), pml4(pml4), pdp(pdp), pd(pd)
 {}
 
+info::~info()
+{
+}
+
 size_t
 info::pages() const
 {
