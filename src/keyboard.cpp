d7d749991dd38ea279704b84bbb3d2fdc3f30023
Nicolas Vivet <nizzox@gmail.com>
2013-03-02 15:28:28 +0100

    Fix keymap declaration

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -5,6 +5,7 @@
 #include "video.h"
 
 keyboard *_keyboard;
+char keymap[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0x0e, 0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0x1c, 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 0, '`', 0, 0, 'z', 'x'    , 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0, 0, ' ', 0};
 
 EXC_FUNC(exc_keyboard, 33)(struct exception_frame *frame)
 {
